cb276845aa5d619a695418efe2463dc67c10e006
karosaxy <araromirichard@gmail.com>
2023-09-20 16:35:07 +0100

    completed

--- app/Http/Controllers/Api/ArticlesController.php
+++ app/Http/Controllers/Api/ArticlesController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\ArticleResource;
 use App\Models\Article;
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class ArticlesController extends Controller
@@ -21,8 +22,29 @@ class ArticlesController extends Controller
 
     public function show(Request $request, Article $article)
     {
+        // Load the article with its associated category
         $article->load(['category:id,name', 'tags:id,name']);
 
-        return new ArticleResource($article);
+        // Get the category ID of the current article
+        $articleCategoryId = $article->category->id;
+
+        // Find other articles with the same category as the current article
+        $relatedArticles = $this->getRelatedArticles($articleCategoryId);
+
+        // Return the current article and the related articles as JSON
+        return response()->json([
+            'current_article' => new ArticleResource($article),
+            'related_articles' => $relatedArticles,
+        ]);
+    }
+
+
+    protected function getRelatedArticles($articleCategoryId)
+    {
+        // Retrieve related articles using the category relationship
+        return Article::where('category_id', $articleCategoryId)
+            ->latest()
+            ->take(5)
+            ->get();
     }
 }
